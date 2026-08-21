2b072aff095ef72fed443fc29eedbed1ec2e0a89
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2026-08-22 07:02:15 +0800

    更新 Convert.py

--- Scripts/Convert.py
+++ Scripts/Convert.py
@@ -58,16 +58,22 @@ RULE_TYPE_MAPPING = {
     "USER-AGENT": {},
     "URL-REGEX": {},
     "PROTOCOL": {},
-    "PROCESS-NAME": {}
+    "PROCESS-NAME": {},
 }
 
 @dataclasses.dataclass(slots=True)
 class Rule:
-    type: str; value: str; param: str = ""
+    type: str
+    value: str
+    param: str = ""
 
 @dataclasses.dataclass(slots=True)
 class RuleSet:
-    name: str; rules: list[Rule] = dataclasses.field(default_factory=list)
+    name: str
+    rules: list[Rule] = dataclasses.field(default_factory=list)
+    @property
+    def total(self):
+        return len(self.rules)
 
 def process_parse(line, enable_type=False, enable_param=False):
     line = DELIMIT_PATTERN.sub(",", COMMENT_PATTERN.sub("", line)).strip()
