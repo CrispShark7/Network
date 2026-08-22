07d89e92563e31cf6587e2123de9b96ddeda3d2d
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2026-08-22 14:55:23 +0800

    更新 Build.py

--- Scripts/Build.py
+++ Scripts/Build.py
@@ -5,26 +5,31 @@ from pathlib import Path
 import Sync
 import Convert
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Rule Build")
     subparsers = parser.add_subparsers(dest="command", required=True)
-    # Sync Source
+
+    # Sync Ruleset
     sync_parser = subparsers.add_parser("S")
     sync_parser.add_argument("repo", nargs="?")
     sync_source = sync_parser.add_mutually_exclusive_group(required=True)
     sync_source.add_argument("--download", dest="mode", action="store_const", const="download")
     sync_source.add_argument("--copy", dest="mode", action="store_const", const="copy")
     sync_parser.set_defaults(handler=sync_mode)
+
     # Convert Ruleset
     convert_parser = subparsers.add_parser("C")
     convert_parser.add_argument("platform", choices=["Egern", "QuantumultX", "Singbox", "Stash", "Surge"])
-    convert_parser.add_argument("file_path", type=Path, nargs="+")
+    convert_parser.add_argument("file_paths", type=Path, nargs="+")
     convert_parser.add_argument("--type", action=argparse.BooleanOptionalAction)
     convert_parser.add_argument("--param", action=argparse.BooleanOptionalAction)
     convert_parser.add_argument("--order", action=argparse.BooleanOptionalAction)
-    convert_parser.add_argument("--unknown-rule", action=argparse.BooleanOptionalAction)
+    convert_parser.add_argument("--exclude", action=argparse.BooleanOptionalAction)
     convert_parser.set_defaults(handler=convert_mode)
+
     return parser.parse_args()
+
 
 def sync_mode(args):
     print("============== Build.py ==============")
@@ -33,18 +38,21 @@ def sync_mode(args):
     print("======================================")
     Sync.process_repo(args.mode, args.repo)
 
+
 def convert_mode(args):
     print("============== Build.py ==============")
     print(f"添加规则类型: {'已启用' if args.type else '未启用'}")
     print(f"添加规则参数: {'已启用' if args.param else '未启用'}")
     print(f"排序规则去重: {'已启用' if args.order else '未启用'}")
-    print(f"未知规则保留: {'已启用' if args.unknown_rule else '未启用'}")
+    print(f"排除规则类型: {'已启用' if args.exclude else '未启用'}")
     print("======================================")
-    Convert.process_file(args.file_path, args)
+    Convert.process_file(args.file_paths, args)
+
 
 def main():
     args = parse_arguments()
     args.handler(args)
+
 
 if __name__ == "__main__":
     main()
