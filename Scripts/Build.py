c7816d2e5ae365a6bf09652ce6dbe6c3597832a7
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2026-09-01 09:20:42 +0800

    更新 Build.py

--- Scripts/Build.py
+++ Scripts/Build.py
@@ -8,21 +8,20 @@ import Convert
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Rule Build")
     subparsers = parser.add_subparsers(dest="command", required=True)
-    # Sync Ruleset
     sync_parser = subparsers.add_parser("S")
     sync_parser.add_argument("repo", nargs="?")
     sync_source = sync_parser.add_mutually_exclusive_group(required=True)
     sync_source.add_argument("--download", dest="mode", action="store_const", const="download")
     sync_source.add_argument("--copy", dest="mode", action="store_const", const="copy")
     sync_parser.set_defaults(handler=sync_mode)
-    # Convert Ruleset
     convert_parser = subparsers.add_parser("C")
-    convert_parser.add_argument("platform", choices=["Egern", "QuantumultX", "Singbox", "Stash", "Surge"])
+    platforms = ["Egern", "QuantumultX", "Singbox", "Stash", "Surge"]
+    convert_parser.add_argument("source_platform", choices=platforms)
+    convert_parser.add_argument("target_platform", choices=platforms)
     convert_parser.add_argument("file_paths", type=Path, nargs="+")
-    convert_parser.add_argument("--type", action=argparse.BooleanOptionalAction)
+    convert_parser.add_argument("--exclude", action=argparse.BooleanOptionalAction)
     convert_parser.add_argument("--param", action=argparse.BooleanOptionalAction)
     convert_parser.add_argument("--order", action=argparse.BooleanOptionalAction)
-    convert_parser.add_argument("--exclude", action=argparse.BooleanOptionalAction)
     convert_parser.set_defaults(handler=convert_mode)
     return parser.parse_args()
 
@@ -35,10 +34,11 @@ def sync_mode(args):
 
 def convert_mode(args):
     print("============== Build.py ==============")
-    print(f"添加规则类型: {'已启用' if args.type else '未启用'}")
-    print(f"添加规则参数: {'已启用' if args.param else '未启用'}")
-    print(f"排序规则去重: {'已启用' if args.order else '未启用'}")
+    print(f"来源规则平台: {args.source_platform}")
+    print(f"目标规则平台: {args.target_platform}")
     print(f"排除规则类型: {'已启用' if args.exclude else '未启用'}")
+    print(f"添加规则参数: {'已启用' if args.param else '未启用'}")
+    print(f"排序规则内容: {'已启用' if args.order else '未启用'}")
     print("======================================")
     Convert.process_files(args.file_paths, args)
 
